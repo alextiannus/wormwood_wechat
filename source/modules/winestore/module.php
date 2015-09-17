a31ef8a303abc48b1b28a4a3b6d64cf8641e592b
gaoshan <mrgaoshan@foxmail.com>
2015-09-17 10:34:50 +0800

    ADD MORE WINE

--- source/modules/winestore/module.php
+++ source/modules/winestore/module.php
@@ -11,17 +11,25 @@ class WinestoreModule extends WeModule {
 	public $table = "idg_pub_wineadmin";
 
 	
-	public function doWineStore(){
+	public function doRequestList(){
 		global $_W,$_GPC;
 		$weid = $_W["weid"];
-		$res = pdo_fetchall("select id,snid,name,FROM_UNIXTIME(creattime)creattime,(CASE  status when 0 then '暂未存酒' else '已经存酒'end)status from ims_wine_store where 1=1 and weid={$weid}");
+		$res = pdo_fetchall("select id,snid,name,FROM_UNIXTIME(creattime)creattime,(CASE  status when 0 then 'Pending' else 'Done'end)status from ims_wine_store where 1=1 and weid={$weid}");
 		include $this->template("wineadmin");
+	}
+	
+	public function doSearchList(){
+	    global $_W,$_GPC;
+	    $weid = $_W["weid"];
+	    $requestNo = $_GPC['requestNo'];
+	    $res = pdo_fetchall("select id,snid,name,FROM_UNIXTIME(creattime)creattime,(CASE  status when 0 then 'Pending' else 'Done'end)status from ims_wine_store where 1=1 and weid={$weid} and snid like '{$requestNo}%' ");
+	    include $this->template("wineadmin");
 	}
 	
 	public function doselect(){
 		global $_GPC;
 		$snid = $_GPC['snid'];
-		$result = pdo_fetchall("select id,snid,name,(CASE type when 1 then '红酒' when 2 then '香槟' when 3 then '洋酒'  else '啤酒'end)type,winename,winenumber,winenum,FROM_UNIXTIME(creattime)creattime,FROM_UNIXTIME(endtime)endtime,(case status when 0 then '未取' else '已取'end)status,remark from ims_wine_store_list where 1=1 and snid={$snid}");
+		$result = pdo_fetchall("select id,snid,cardnumber,(CASE type when 1 then '红酒' when 2 then '香槟' when 3 then '洋酒'  else '啤酒'end)type,amount,winename,winenumber,winenum,FROM_UNIXTIME(creattime)creattime,FROM_UNIXTIME(endtime)endtime,collectedby,(case status when 0 then '未取' when 2 then '待取' else '已取'end)status,remark from ims_wine_store_list where 1=1 and snid={$snid}");
 		include $this->template("select");
 	}
 	
@@ -49,12 +57,12 @@ class WinestoreModule extends WeModule {
 			if($status){
 				$detailstatus= pdo_query("delete from ims_wine_store_list where 1=1 and snid = {$snid}");
 				if($detailstatus){
-				$url = $this->createWebUrl('wineStore');  
+				$url = $this->createWebUrl('requestList');  
 				echo "<script language='javascript' type='text/javascript'>alert('删除成功,点击确定跳转!');";  
 				echo "window.location.href='$url'";  
 				echo "</script>"; 
 				}else{
-				    $url = $this->createWebUrl('wineStore');
+				    $url = $this->createWebUrl('requestList');
 				echo "<script language='javascript' type='text/javascript'>alert('删除成功,该用户没有存酒!');"; 
 				echo "window.location.href='$url'";
 				echo "</script>"; 
@@ -77,16 +85,29 @@ class WinestoreModule extends WeModule {
 		$remark = $_GPC['remark'];
 		$snid = $_GPC['snid'];
 		$name = $_GPC['name'];
+		$amount = $_GPC['amount'];
 		$time = time();
 	
 		for($i=0;$i<$arr_num;$i++){
+		  
+		    while (true) {
+		        $rand =rand(9000001,9999999);
+		        $cardnumber="SCF".strval($rand);
+		        $check= pdo_fetch('SELECT COUNT(1) AS num from'.tablename('wine_store_list')." WHERE cardnumber = :cardnumber" ,array(':cardnumber'=>$cardnumber));
+		        if($check['num']==0){
+		            break;
+		        }
+		    }
+		
 		    $data = array(
 		        'snid' => $snid,
+		        'cardnumber' => $cardnumber,
 		        'winename' => $winename[$i],
 		        'winenumber' => $winenumber[$i],
 		        'winenum' =>$winenum[$i],
 		        'creattime' =>$time,
 		        'type' => $type[$i],
+		        'amount' => $amount[$i],
 		        'remark' => $remark[$i]
 		    );
 		  $status = pdo_insert('wine_store_list',$data);
@@ -105,7 +126,7 @@ class WinestoreModule extends WeModule {
 			echo "</script>";
 		
 		}else{
-		$url = $this->createWebUrl('wineStore');  
+		$url = $this->createWebUrl('requestList');  
 		echo "<script language='javascript' type='text/javascript'>alert('执行成功,点击确定跳转!');";  
 		echo "window.location.href='$url'";  
 		echo "</script>"; 
@@ -129,9 +150,10 @@ class WinestoreModule extends WeModule {
         $winename= $_GPC['winename'];
         $winenumber = $_GPC['winenumber'];
         $winenum = $_GPC['winenum'];
+        $amount = $_GPC['amount'];
         $remark = $_GPC['remark'];
         
-        $sql = pdo_query("update ims_wine_store_list set type = {$type},winename='{$winename}',winenumber={$winenumber},winenum={$winenum},remark='{$remark}'  where 1=1 and id={$id}");
+        $sql = pdo_query("update ims_wine_store_list set type = {$type},winename='{$winename}',winenumber={$winenumber},winenum={$winenum},remark='{$remark}' ,amount={$amount} where 1=1 and id={$id}");
         if($sql===false){
 			echo "<script language='javascript' type='text/javascript'>alert('(ims_wine_store_list)System Error,Please Try Again!');"; 
 			echo "</script>";
@@ -144,14 +166,24 @@ class WinestoreModule extends WeModule {
 			
 		}
     }
+    //填写取酒信息
+    public function doRetrieveWine(){
+        global $_W,$_GPC;
+        $id=$_GPC['id'];
+        $snid=$_GPC['snid'];
+        $result = array('id'=>$id,"snid"=>$snid);
+        include $this->template("retrieveWine");
+    }
 
-
-	public function doupdatabj(){
+//更新取酒信息
+	public function doUpdateStatus(){
 		global $_W,$_GPC;
 		$id=$_GPC['id'];
 		$snid=$_GPC['snid'];
+		$collectedby=$_GPC['collectedby'];
+		$remark=$_GPC['remark'];
 		$time = time();
-		$status = pdo_query("update ims_wine_store_list set status=1 ,endtime={$time} where 1=1 and id={$id}");
+		$status = pdo_query("update ims_wine_store_list set status=1 ,endtime={$time},collectedby='{$collectedby}',remark='{$remark}' where 1=1 and id={$id}");
 		if($status==FALSE){
 			echo "<script language='javascript' type='text/javascript'>alert('Error,Please Try Again!');"; 
 			echo "</script>";
@@ -169,7 +201,7 @@ class WinestoreModule extends WeModule {
 		$id=$_GPC['id'];
 		$snid=$_GPC['snid'];
 		$status = pdo_query("delete from ims_wine_store_list where 1=1 and id={$id} ");
-		if($status==FALSEl){
+		if($status==FALSE){
 			echo "<script language='javascript' type='text/javascript'>alert('Error,Please Try Again!');"; 
 			echo "</script>";
 		}else{
@@ -180,7 +212,69 @@ class WinestoreModule extends WeModule {
 			
 		}
 	}
-
+	
+	
+	public function doWineCollectList(){
+	    global $_W,$_GPC;
+	    $res = pdo_fetchall("select id,cardnumber,winename,collectedby,tablenumber,FROM_UNIXTIME(creattime)creattime,(CASE  status when 0 then 'Pending' else 'Done'end)status from ims_wine_retrieve_list where 1=1 ");
+	    include $this->template("collectList");
+	}
+	
+	public function doWineCollectSearch(){
+	    global $_W,$_GPC;
+	    $cardnumber=$_GPC['cardnumber'];
+	    $res = pdo_fetchall("select id,cardnumber,winename,collectedby,tablenumber,FROM_UNIXTIME(creattime)creattime,(CASE  status when 0 then 'Pending' else 'Done'end)status from ims_wine_retrieve_list where 1=1 and cardnumber like '{$cardnumber}%' ");
+	    include $this->template("collectList");
+	}
+	
+	public function doUpdateCollect(){
+	    global $_W,$_GPC;
+	    $cardnumber=$_GPC['cardnumber'];
+	    $collectedby=$_GPC['collectedby'];
+	    
+	    $status = pdo_query("update ims_wine_retrieve_list set status=1  where 1=1 and cardnumber='{$cardnumber}' ");
+	    if($status==FALSE){
+	        echo "<script language='javascript' type='text/javascript'>alert('Error,Please Try Again!');";
+	        echo "</script>";
+	    }else{
+	        $status = pdo_query("update ims_wine_store_list set status=1 ,collectedby='{$collectedby}' where 1=1 and cardnumber='{$cardnumber}' ");
+	        if($status==FALSE){
+	            echo "<script language='javascript' type='text/javascript'>alert('Error Update wine_store_list ,Please Try Again!');";
+	            echo "</script>";
+	        }else{
+	            $url = $this->createWebUrl('wineCollectList');
+	            echo "<script language='javascript' type='text/javascript'>alert('更新成功,点击确定跳转!');";
+	            echo "window.location.href='$url'";
+	            echo "</script>";
+	            
+	        }
+	        
+	      
+	        	
+	    }
+	}
+	
+	public function doWineStorageList(){
+	    global $_W,$_GPC;
+	   	$result = pdo_fetchall("select id,snid,cardnumber,(CASE type when 1 then '红酒' when 2 then '香槟' when 3 then '洋酒'  else '啤酒'end)type,amount,winename,winenumber,winenum,FROM_UNIXTIME(creattime)creattime,FROM_UNIXTIME(endtime)endtime,collectedby,(case status when 0 then '未取' when 2 then '待取' else '已取'end)status,remark from ims_wine_store_list where 1=1 ");
+	    include $this->template("storageList");
+	}
+	
+	public function doWineStorageSearch(){
+	    global $_W,$_GPC;
+	    $cardnumber=$_GPC['cardnumber'];
+	    $snid=$_GPC['snid'];
+	    $where="";
+	    if(!empty($cardnumber)){
+	        $where.=" AND cardnumber like '{$cardnumber}%' ";
+	    }
+	    if(!empty($cardnumber)){
+	        $where.=" AND snid like '{$snid}%' ";
+	    }
+	   
+	    $result = pdo_fetchall("select id,snid,cardnumber,(CASE type when 1 then '红酒' when 2 then '香槟' when 3 then '洋酒'  else '啤酒'end)type,amount,winename,winenumber,winenum,FROM_UNIXTIME(creattime)creattime,FROM_UNIXTIME(endtime)endtime,collectedby,(case status when 0 then '未取' when 2 then '待取' else '已取'end)status,remark from ims_wine_store_list where 1=1 {$where} ");
+	    include $this->template("storageList");
+	}
 	
 	
 	
--- source/modules/winestore/site.php
+++ source/modules/winestore/site.php
@@ -7,144 +7,151 @@
  */
 defined('IN_IA') or exit('Access Denied');
 
+class WinestoreModuleSite extends WeModuleSite
+{
 
-class WinestoreModuleSite extends WeModuleSite {
-	
     public $getUserInfoUrl = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token=%s&openid=%s&lang=zh_CN';
 
+    private function getUserInfo($o)
+    {
+        global $_W, $_GPC;
+        checkaccount();
+        if (empty($o)) {
+            message('重要参数丢失..', '', 'error');
+            exit();
+        }
+        if (empty($_W['account']['key']) || empty($_W['account']['secret'])) {
+            $user = pdo_fetch('SELECT from_user,weid,nickname,avatar FROM' . tablename('fans') . "WHERE weid = :weid AND from_user = :from_user", array(
+                ':from_user' => $o,
+                ':weid' => $_W['weid']
+            ));
+            if (empty($user)) {
+                $user = array(
+                    'weid' => $_W['weid'],
+                    'from_user' => $o,
+                    'createtime' => TIMESTAMP
+                );
+                /* pdo_insert('fans',$user); */
+            }
+            if (empty($user['nickname'])) {
+                $user['nickname'] = 'Anonymous-' . substr($o, - 4);
+            }
+            if (empty($user['avatar'])) {
+                $user['avatar'] = './source/modules/winestore/template/style/null_header.png';
+            }
+            return $user;
+        } else {
+            $access_token = account_weixin_token($_W['account']);
+            $content = ihttp_get(sprintf($this->getUserInfoUrl, $access_token, $o));
+            if ($content['errcode'] != 0) {
+                message(account_weixin_code($content['errcode']), '', 'error');
+            }
+            $record = @json_decode($content['content'], true);
+            if ($record['subscribe'] == '1') {
+                $user = array(
+                    'weid' => $_W['weid'],
+                    'from_user' => $record['openid'],
+                    'nickname' => $record['nickname'],
+                    'gender' => $record['sex'],
+                    'residecity' => $record['city'],
+                    'resideprovince' => $record['province'],
+                    'nationality' => $record['country'],
+                    'avatar' => $record['headimgurl'],
+                    'createtime' => $record['subscribe_time']
+                );
+                
+                if (pdo_fetch("SELECT * FROM " . tablename('fans') . " WHERE `from_user` = '{$record['openid']}'")) {
+                    pdo_update('fans', $user, array(
+                        'from_user' => $record['openid']
+                    ));
+                } else {
+                    pdo_insert('fans', $user);
+                }
+            }
+            if (empty($user['nickname'])) {
+                $user['nickname'] = 'Anonymous_' . substr($o, - 4);
+            }
+            if (empty($user['avatar'])) {
+                $user['avatar'] = './source/modules/winestore/template/style/null_header.png';
+            }
+            return $user;
+        }
+    }
 
-	private function getUserInfo($o){
-		global $_W,$_GPC;
-		checkaccount();
-		if(empty($o)){
-			message('重要参数丢失..','','error');
-			exit();
-		}
-		if(empty($_W['account']['key'])||empty($_W['account']['secret'])){
-			$user = pdo_fetch('SELECT from_user,weid,nickname,avatar FROM'.tablename('fans'). "WHERE weid = :weid AND from_user = :from_user",array(':from_user'=>$o,':weid'=>$_W['weid']));
-			if(empty($user)){
-				$user = array(
-							'weid' => $_W['weid'],
-							'from_user' => $o,
-							'createtime'=> TIMESTAMP,
-							);
-				/*pdo_insert('fans',$user);*/
-			}
-			if(empty($user['nickname'])){
-					$user['nickname'] = 'Anonymous-'.substr($o, -4);
-				}
-			if(empty($user['avatar'])){
-					$user['avatar'] = './source/modules/winestore/template/style/null_header.png';
-				}
-			return $user;
-		}else{
-			$access_token = account_weixin_token($_W['account']);
-			$content = ihttp_get(sprintf($this->getUserInfoUrl,$access_token,$o));
-			if($content['errcode']!=0){
-				message(account_weixin_code($content['errcode']),'','error');
-			}
-			$record = @json_decode($content['content'], true);
-			if ($record['subscribe'] == '1') {
-				$user = array(
-					'weid' => $_W['weid'],
-					'from_user' => $record['openid'],
-					'nickname' => $record['nickname'],
-					'gender' => $record['sex'],
-					'residecity'=> $record['city'],
-					'resideprovince' => $record['province'],
-					'nationality' => $record['country'],
-					'avatar' => $record['headimgurl'],
-					'createtime'=> $record['subscribe_time']
-					);
-               
-				if (pdo_fetch("SELECT * FROM ".tablename('fans')." WHERE `from_user` = '{$record['openid']}'")) {
-					pdo_update('fans', $user, array('from_user' => $record['openid']));
-				}else{
-					pdo_insert('fans',$user);
-				}
-			}
-			if(empty($user['nickname'])){
-					$user['nickname'] = 'Anonymous_'.substr($o, -4);
-				}
-			if(empty($user['avatar'])){
-					$user['avatar'] = './source/modules/winestore/template/style/null_header.png';
-				}
-			return $user;
-		}
-	}
+    public function doMobileEntrance()
+    {
+        global $_W, $_GPC;
+        
+        // $user_agent = $_SERVER ['HTTP_USER_AGENT'];
+        // if (strpos ( $user_agent, 'MicroMessenger' ) === false) {
+        // echo "本页面仅支持微信访问!非微信浏览器禁止浏览!";
+        // exit ();
+        // }
+        
+        $fansid = $_W['fans']['from_user'];
+        $status = pdo_fetch("select fansid from " . tablename('wine_store') . "where 1=1 and fansid = '{$fansid}'");
+        
+        if (! empty($status)) {
+            $result = pdo_fetch("select snid,FROM_UNIXTIME(creattime)creattime,(CASE  status when 0 then 'No stock ' else 'Finish' end)status from" . tablename('wine_store') . "where 1=1 and fansid='{$fansid}'");
+            
+            include $this->template('winetrue');
+        } else {
+            include $this->template('wine');
+        }
+    }
 
-
-    public function doMobileEntrance(){
-		global $_W,$_GPC;
-		
-		
-// 		$user_agent = $_SERVER ['HTTP_USER_AGENT'];
-// 		if (strpos ( $user_agent, 'MicroMessenger' ) === false) {
-// 			echo "本页面仅支持微信访问!非微信浏览器禁止浏览!";
-// 			exit ();
-// 		}
-		
-		$fansid=$_W['fans']['from_user'];
-		$status = pdo_fetch("select fansid from ".tablename('wine_store')."where 1=1 and fansid = '{$fansid}'");	
-		
-		if(!empty($status)){
-		$result = pdo_fetch("select snid,FROM_UNIXTIME(creattime)creattime,(CASE  status when 0 then 'No stock ' else 'Finish' end)status from".tablename('wine_store')."where 1=1 and fansid='{$fansid}'");
-		
-		include $this->template('winetrue');
-		
-		}else{
-			include $this->template('wine');
-		}
-	}
-	
-	public function doMobileStore(){
-		global $_W,$_GPC;
-		
-		$weid = $_GPC['weid'];
-		//$weid=5;
-		
-		$time = date('Y-m-d H:i:s',time());
-		$time1=time();
-		$fansid=$_W['fans']['from_user'];
-		//$fansid='dddaaaa11';
-		
-		$name = $this->getUserInfo($fansid);
-		$username = $name['nickname'];
-		//$username='test12345';
-		
-		
-		$status = pdo_fetch("select fansid from ".tablename('wine_store')." where 1=1 and fansid = :fansid and weid = :weid",array(':fansid'=>$fansid,':weid'=>$_W['weid']));	
-		if(!empty($status)){
-		    $jd['IsSuccess']=false;
-		    echo json_encode($jd);
-			exit;
-		}
-		while (true) {
-			$rand =rand(100001,999998);
-			$isok = pdo_fetch('SELECT COUNT(*) AS num from'.tablename('wine_store')." WHERE snid = :snid" ,array(':snid'=>$rand));
-			if($isok['num']==0){
-				break;
-			}
-		}
-		
-		$status = pdo_query("insert into ".tablename('wine_store')."(snid,fansid,creattime,name,weid) values('{$rand}','{$fansid}',{$time1},'{$username}','{$weid}')");
-		
-		
-		if($status==FALSE){
-		    $jd['IsSuccess']=false;
-		    echo json_encode($jd);
-			}else{
-				
-			$jd['div'] = "
+    public function doMobileStore()
+    {
+        global $_W, $_GPC;
+        
+        $weid = $_GPC['weid'];
+        // $weid=5;
+        
+        $time = date('Y-m-d H:i:s', time());
+        $time1 = time();
+        $fansid = $_W['fans']['from_user'];
+        // $fansid='dddaaaa11';
+        
+        $name = $this->getUserInfo($fansid);
+        $username = $name['nickname'];
+        // $username='test12345';
+        
+        $status = pdo_fetch("select fansid from " . tablename('wine_store') . " where 1=1 and fansid = :fansid and weid = :weid", array(
+            ':fansid' => $fansid,
+            ':weid' => $_W['weid']
+        ));
+        if (! empty($status)) {
+            $jd['IsSuccess'] = false;
+            echo json_encode($jd);
+            exit();
+        }
+        while (true) {
+            $rand = rand(100001, 999998);
+            $isok = pdo_fetch('SELECT COUNT(*) AS num from' . tablename('wine_store') . " WHERE snid = :snid", array(
+                ':snid' => $rand
+            ));
+            if ($isok['num'] == 0) {
+                break;
+            }
+        }
+        
+        $status = pdo_query("insert into " . tablename('wine_store') . "(snid,fansid,creattime,name,weid) values('{$rand}','{$fansid}',{$time1},'{$username}','{$weid}')");
+        
+        if ($status == FALSE) {
+            $jd['IsSuccess'] = false;
+            echo json_encode($jd);
+        } else {
+            
+            $jd['div'] = "
             </br></br><div id='deposit-card'>
             <ul>
                     <li>
-                        <p>Card No. ：</p>
+                        <p>Request No. ：</p>
                         <p>{$rand}</p>
                     </li>
                     <li>
                         <p>Directions：</p>
-                        <p>Please go to the front desk, submit the card number for storing wine </p>
+                        <p>Please go to the front desk, submit the request number for storing wine </p>
                     </li>
                     <li>
                         <p>Status：</p>
@@ -156,13 +163,81 @@ class WinestoreModuleSite extends WeModuleSite {
                     </li>                         
 				</ul>
 			</div>";
-		$jd['IsSuccess']=true;
-		$jd['code']=$rand;
-			echo json_encode($jd);
-		}
-	
-		 
-		 
-		
-	}
+            $jd['IsSuccess'] = true;
+            $jd['code'] = $rand;
+            echo json_encode($jd);
+        }
+    }
+
+    public function doMobileWineList()
+    {
+        global $_W, $_GPC;
+        $fansid = $_W['fans']['from_user'];
+        
+        $snid = $_GPC['snid'];
+        
+        $result = pdo_fetchall("select id,snid,cardnumber,amount,winename,winenumber,winenum,FROM_UNIXTIME(creattime)creattime,FROM_UNIXTIME(endtime)endtime,collectedby,remark from ims_wine_store_list where 1=1 and status=0 and snid={$snid}");
+        
+        include $this->template('wineList');
+    }
+
+    public function doMobileCollectWineList()
+    {
+        global $_W, $_GPC;
+        $fansid = $_W['fans']['from_user'];
+        
+        $ids = $_GPC['ids'];
+        
+        include $this->template('submitWine');
+    }
+
+    public function doMobileSubmitWine()
+    {
+        global $_W, $_GPC;
+        
+        $ids = $_GPC['ids'];
+        
+        $list_ids = explode(",", $ids);
+        
+        $tablenumber = $_GPC['tablenumber'];
+        
+        $collectedby = $_GPC['collectedby'];
+        
+        $time = date('Y-m-d H:i:s', time());
+        $time1 = time();
+        
+        foreach ($list_ids as $item_id) {
+            
+                $result = pdo_fetch("select * from" . tablename('wine_store_list') . "where 1=1 and status=0 and id='{$item_id}'");
+                
+                if(empty($result)){
+                    echo json_encode(array(
+                        'info' => 'You Have Already Submitted.'
+                    ));
+                    exit();
+                }
+                $updatestatus = pdo_query("update ims_wine_store_list set status=2 where 1=1 and id={$item_id}");
+                
+                $data = array(
+                    'cardnumber' => $result['cardnumber'],
+                    'winename' => $result['winename'],
+                    'collectedby' => $collectedby,
+                    'tablenumber' => $tablenumber,
+                    'creattime' => $time1
+                );
+                
+                $status = pdo_insert('wine_retrieve_list', $data);
+                
+                if ($status == FALSE) {
+                    echo json_encode(array(
+                        'info' => 'System Error.'
+                    ));
+                    exit();
+                }
+        }
+        
+        echo json_encode(array(
+            'info' => 'Your Information has been submitted succesffully.'
+        ));
+    }
 }
